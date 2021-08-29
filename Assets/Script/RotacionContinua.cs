3ced901389e99f9570d33eb3e443d6c68c6c90b4
JDSucunza <jdsucunza@gmail.com>
2021-08-29 19:10:35 -0300

    Create RotacionContinua.cs

---
+++ Assets/Script/RotacionContinua.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotacionContinua : MonoBehaviour
+{
+    
+    public float rotacionX;
+    public float rotacionY;
+    public float rotacionZ;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate (rotacionX * Time.deltaTime, rotacionY * Time.deltaTime, rotacionZ * Time.deltaTime);
+    }
+}
