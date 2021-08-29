6d4fe6ddf0f164a4fe986a4b18199638310a18a8
JDSucunza <jdsucunza@gmail.com>
2021-08-29 19:28:29 -0300

    Create DispararBala.cs

---
+++ Assets/Script/DispararBala.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DispararBala : MonoBehaviour
+{
+    
+    public GameObject balaClonada;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown ("DispararBala")){
+            Instantiate (balaClonada);
+        }
+    }
+}
