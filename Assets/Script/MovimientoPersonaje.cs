3a60721face8e319b619c1b4b47a693cc4facce7
JDSucunza <jdsucunza@gmail.com>
2021-08-29 20:06:02 -0300

    Update MovimientoPersonaje.cs

--- Assets/Script/MovimientoPersonaje.cs
+++ Assets/Script/MovimientoPersonaje.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class MovimientoPersonaje : MonoBehaviour
 {
+    
+    public float velocidadMovimiento;
+    public float velocidadRotacion;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,17 @@ public class MovimientoPersonaje : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButton("Arriba")){
+            transform.Translate (0,0,velocidadMovimiento * Time.deltaTime);
+        }
+        if (Input.GetButton("Abajo")){
+            transform.Translate (0,0,- velocidadMovimiento * Time.deltaTime);
+        }
+        if (Input.GetButton ("Izquierda")){
+            transform.Rotate (0, - velocidadRotacion * Time.deltaTime, 0);
+        }
+        if (Input.GetButton ("Derecha")){
+            transform.Rotate (0, velocidadRotacion * Time.deltaTime, 0);
+        }
     }
 }
