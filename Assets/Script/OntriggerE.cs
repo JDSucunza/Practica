873efd3d574f465f240a1f93e22b96989ac767e7
JDSucunza <jdsucunza@gmail.com>
2021-09-01 00:00:20 -0300

    Create OntriggerE.cs

---
+++ Assets/Script/OntriggerE.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OntriggerE : MonoBehaviour
+{
+    
+    public GameObject objetivo;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter (){
+        Destroy (objetivo);
+    }
+}
