5b58d9d61ee0eb46a8860bfb0799884995e778e9
JDSucunza <jdsucunza@gmail.com>
2021-09-08 06:29:36 -0300

    Create Rigbody.cs

---
+++ Assets/Script/Rigbody.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rigbody : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter (Collision c){
+        Rigidbody rb = c.gameObject.GetComponent <Rigidbody>();
+        rb.AddRelativeForce (0,0,100);
+    }
+}
