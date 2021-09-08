110ed1b7c39d3460a9994ec7314b23e7fbe48b9b
JDSucunza <jdsucunza@gmail.com>
2021-09-08 06:29:32 -0300

    Create triggerPuerta.cs

---
+++ Assets/Script/triggerPuerta.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerPuerta : MonoBehaviour
+{
+    
+    public GameObject objetivo;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter (){
+        objetivo.transform.Translate (0,0,10);
+    }
+}
