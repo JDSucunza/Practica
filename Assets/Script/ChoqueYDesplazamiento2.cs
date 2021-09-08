9cfb48da35c155b5ccda5927d6eb9ba4dc920c64
JDSucunza <jdsucunza@gmail.com>
2021-09-08 06:29:48 -0300

    Create ChoqueYDesplazamiento2.cs

---
+++ Assets/Script/ChoqueYDesplazamiento2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoqueYDesplazamiento2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter (Collision c){
+        c.gameObject.transform.position = c.gameObject.transform.position + c.gameObject.transform.forward ;
+    }
+}
