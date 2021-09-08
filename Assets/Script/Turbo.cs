f0ef604c4c89017a983f8b4aededb7c961bd6e2b
JDSucunza <jdsucunza@gmail.com>
2021-09-08 06:29:27 -0300

    Create Turbo.cs

---
+++ Assets/Script/Turbo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turbo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter (Collider c){
+        MovimientoPersonaje m = c.gameObject.GetComponent <MovimientoPersonaje>();
+        m.velocidadMovimiento= m.velocidadMovimiento + 50;
+        Destroy (gameObject); 
+    }
+}
