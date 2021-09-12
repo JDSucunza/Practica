536c57f13bb5c32ba0234933eccefaf90f669d13
JDSucunza <jdsucunza@gmail.com>
2021-09-12 19:53:13 -0300

    Create QuitarVida.cs

---
+++ Assets/Script/QuitarVida.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitarVida : MonoBehaviour
+{
+    
+    public float danio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // REVISAR!!!!
+    void OnCollisionEnter (Collision c){
+        Vida vidaDelObjeto = c.gameObject.GetComponent <Vida>();
+        if (vidaDelObjeto.escudo >= danio){
+            vidaDelObjeto.escudo = vidaDelObjeto.escudo - danio;
+        } else {
+            vidaDelObjeto.cantidad = vidaDelObjeto.cantidad - danio;
+        }
+    }
+}
