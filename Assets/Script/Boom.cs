1e14808daf41bb28d9b613219ae528e8fda8ea18
JDSucunza <jdsucunza@gmail.com>
2021-09-13 19:00:26 -0300

    Create Boom.cs

---
+++ Assets/Script/Boom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boom : MonoBehaviour
+{
+    
+    public float tiempo;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Autodestruir (){
+        Destroy (gameObject);
+    }
+
+    public void awake (){
+        Invoke ("Autodestruir", tiempo);
+    }
+}
