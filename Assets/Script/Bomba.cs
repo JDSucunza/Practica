066ed2f099bb2a99700667ddc09e34709a035327
JDSucunza <jdsucunza@gmail.com>
2021-09-15 14:45:32 -0300

    Update Bomba.cs

--- Assets/Script/Bomba.cs
+++ Assets/Script/Bomba.cs
@@ -6,6 +6,12 @@ public class Bomba : MonoBehaviour
 {
     
     public GameObject jugador;
+    private bool explotar = false;
+    
+    
+    void Awake (){
+        Invoke ("Explotar", 2);
+    }
     
     // Start is called before the first frame update
     void Start()
@@ -18,6 +24,19 @@ public class Bomba : MonoBehaviour
     {
         if (Input.GetButtonDown ("Explotar")){
             Destroy (jugador);
+            explotar = true;
         }
     }
+
+    void Explotar (){
+        explotar = true;
+    }
+
+    void OnTriggerStay (Collider c){
+        if (explotar){
+            Destroy (c.gameObject);
+            Destroy (this.gameObject);
+        }
+    }
+
 }
