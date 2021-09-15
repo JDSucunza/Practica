a702ccbe6f765af2fafe5d975d9da4c5a395f003
JDSucunza <jdsucunza@gmail.com>
2021-09-15 14:45:02 -0300

    Create GeneradorOleadas.cs

---
+++ Assets/Script/GeneradorOleadas.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneradorOleadas : MonoBehaviour
+{
+    
+    public float tiempo;
+    public GameObject prefab;
+    
+    void Awake (){
+        InvokeRepeating ("Respawn", tiempo, 0.3f);
+    } 
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Respawn (){
+        Instantiate (prefab, this.transform.position, this.transform.rotation);
+    }
+}
