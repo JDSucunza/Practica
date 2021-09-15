e9540078ff9e94b4e0cfb1425e454578d5313ab7
JDSucunza <jdsucunza@gmail.com>
2021-09-15 14:45:26 -0300

    Create DesctructorTemp.cs

---
+++ Assets/Script/DesctructorTemp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesctructorTemp : MonoBehaviour
+{
+    
+    public int tiempo;
+
+    void Awake (){
+        Invoke ("Destruir", tiempo );
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Destruir (){
+        Destroy (this.gameObject);
+    }
+
+}
