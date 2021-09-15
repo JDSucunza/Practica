46c8ab57a6e308d0694feb8d6106e05c55606944
JDSucunza <jdsucunza@gmail.com>
2021-09-15 14:45:20 -0300

    Create EjManager1.cs

---
+++ Assets/Script/EjManager1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjManager1 : MonoBehaviour
+{
+    
+    public int numero;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (numero == 10){
+            print ("Hola");
+        }
+    }
+}
