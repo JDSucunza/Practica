af8adceda7b5b175af5911a30b2ed4bfe053e753
JDSucunza <jdsucunza@gmail.com>
2021-09-15 14:44:50 -0300

    Create EjObjeto1.cs

---
+++ Assets/Script/EjObjeto1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjObjeto1 : MonoBehaviour
+{
+    
+    public GameObject manager;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown ("Fire1")){
+             private int gameManager = manager.GetComponent <EjManager1>();
+           // gameManager.numero = gameManager.numero + 1;
+        }
+    }
+}
