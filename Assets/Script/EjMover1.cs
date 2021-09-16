7ba4d075fd9351c594ca338cb15be94fa6efcbee
JDSucunza <jdsucunza@gmail.com>
2021-09-16 02:56:02 -0300

    Create EjMover1.cs

---
+++ Assets/Script/EjMover1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjMover1 : MonoBehaviour
+{
+    
+    public Rigidbody objetivo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown ("Explotar")){
+            objetivo.AddRelativeForce (0,0,100);
+        }
+    }
+}
