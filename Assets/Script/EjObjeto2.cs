76b3d336f160b3184f2bfa331503c21e79feb1f1
JDSucunza <jdsucunza@gmail.com>
2021-09-16 02:55:55 -0300

    Create EjObjeto2.cs

---
+++ Assets/Script/EjObjeto2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjObjeto2 : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    
+    
+    void BuscarManager (){
+        GameOBject manager = GameObject.Find ("Manager");
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown ("jump")){
+            EjManager1 enemyManager = manager.gameObject.GetComponent <EjManager1>();
+            enemyManager.numero = enemyManager.numero + 1;
+        }
+    }
+
+
+}
